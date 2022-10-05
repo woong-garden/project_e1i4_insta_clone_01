984e89e4bb8bbdcefdc19e47c6dec2454cf20c6f
auberr <auberr@daum.net>
2022-10-06 00:17:47 +0900

    debug mode True

--- e1i4/settings.py
+++ e1i4/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l!nhqf=$mq#60p-8=^-!)&n-j3n-a$nf7ag%h(jlevbanl08%9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['15.164.219.140', '127.0.0.1', 'http://archivearmy.shop/']
+# ALLOWED_HOSTS = ['15.164.219.140', '127.0.0.1', 'http://archivearmy.shop/']
 
 
 # Application definition
