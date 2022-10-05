0e2ee019e85c7ba1c133673927a878a7a8cb734a
auberr <auberr@daum.net>
2022-10-06 00:12:26 +0900

    allowed_hosts

--- e1i4/settings.py
+++ e1i4/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l!nhqf=$mq#60p-8=^-!)&n-j3n-a$nf7ag%h(jlevbanl08%9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['15.164.219.140']
+ALLOWED_HOSTS = ['15.164.219.140', '127.0.0.1', 'http://archivearmy.shop/']
 
 
 # Application definition
