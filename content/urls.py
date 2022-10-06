1e3cc17f36b24583bc70df3454db5e99b52a6c09
auberr <auberr@daum.net>
2022-10-06 12:49:07 +0900

    update

--- content/urls.py
+++ content/urls.py
@@ -15,9 +15,9 @@ urlpatterns = [
     path('<int:id>/delete/', delete_feed), # 게시글 삭제
     path('<int:id>/update/', update_feed), # 게시글 수정
     # 1번 방식
-    path('<int:pk>', views.detail, name='detail'),
+    # path('<int:id>', Main.detail, name='detail'),
     # 2번 방식
-    # path('<int:pk>', Main.detail),
+    path('<int:pk>', views.detail, name='detail'),
    
     path('delete/<int:id>/', delete_reply, name='delete'),
     
